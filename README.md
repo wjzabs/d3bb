bb00c03fc5b1a147aa57b269eab75a43d6a3a523
Walter J. Zielenski <wjz@absolution.com>
2024-09-21 15:05:17 -0400

    map using sales by fips, color coded

--- README.md
+++ README.md
@@ -53,8 +53,14 @@ https://github.com/topojson/us-atlas/tree/master/img
 US Census
 https://www.census.gov/
 
+sdwfrost gist & github
+https://gist.githubusercontent.com/sdwfrost/d1c73f91dd9d175998ed166eb216994a/raw/e89c35f308cee7e2e5a784e1d3afc5d449e9e4bb/counties.geojson
 
-FIPS Master List
+*** US Census - cb_2017_us_county_20m - this is the source of the counties geometries for the map
+https://www.census.gov/geographies/mapping-files/2017/geo/carto-boundary-file.html
+
+
+*** FIPS Master List - this is the source of FIPS master data - xls file
 https://public.opendatasoft.com/explore/dataset/us-county-boundaries/export/?disjunctive.statefp&disjunctive.countyfp&disjunctive.name&disjunctive.namelsad&disjunctive.stusab&disjunctive.state_name
 
 Plotly dataasets
@@ -63,7 +69,7 @@ https://github.com/plotly/datasets/commit/95672208c26b44a6e32363b17a35b8caa1b5d2
 reddit advice on zip to fips
 https://www.reddit.com/r/Census/comments/llcsvk/census_block_fips_code_to_zip_code/
 
-zip to fips
+zip to fips - could not easily find what I was looking for - but a treasure trove of detailed info
 https://www.censushardtocountmaps2020.us/?latlng=39.68183%2C-79.43115&z=6&query=coordinates%3A%3A40.61395%2C-72.99316&promotedfeaturetype=states&baselayerstate=4&rtrYear=sR2020latest&infotab=info-rtrselfresponse&filterQuery=false
 
 xref zip to fips
@@ -75,5 +81,83 @@ https://www.huduser.gov/portal/datasets/usps_crosswalk.html
 12 cross walk files
 https://www.huduser.gov/apps/public/uspscrosswalk/home
 
+*** List of State Codes and County FIPS Codes- this is the source of State FIPS prefix -> State Code master data
+https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt
 
 
+select JSON_OBJECT(GEO_POINT,
+GEO_SHAPE,STATEFP,COUNTYFP,COUNTYNS,GEOID,NAME,NAMELSAD,STUSAB,LSAD,CLASSFP,MTFCC,
+CSAFP,CBSAFP,METDIVFP,FUNCSTAT,ALAND,AWATER,INTPTLAT,INTPTLON,STATE_NAME,COUNTYFP_NOZERO)
+ from GEODATA
+
+
+*** List of State FIPS -> Code & Name - this is the source of State FIPS xref
+https://gist.github.com/dantonnoriega/bf1acd2290e15b91e6710b6fd3be0a53
+
+Converting Zip to FIPS is a bit problemmatic
+file ZIP-COUNTY-FIPS_2017-06.CSV shows 2 FIPS for zip code 23061
+ZIP	    COUNTYNAME	        ST  COUNTYFP	CLASSFP
+23061	Gloucester County	VA	51073	    H1
+23061	Mathews County	    VA	51115	    H1
+
+Smarty has an API lookup that you provide City, State and Zip code and it returns the reall FIPS
+https://www.smarty.com/products/single-address?city=&state=&zipcode=&address-type=us-zipcode
+ZIP Code Type S
+Default City Gloucester
+County Name Gloucester
+County FIPS 51073
+State Virginia
+Latitude 37.42635
+Longitude -76.54519
+Lat/Long Precision Zip5
+above shows it worked for Gloucester, but Mathews returned 0 rows.
+
+
+we could also use zip2fips which includes PR
+and had 1 resolution for 23061 (Gloucester) with Mathews in its own zip code (23109)
+*** this is the resource we selected to use for zip 2 fips - it was loaded into oracle and used to obtain sample retail sales data by fips
+https://github.com/clauswilke/zipcodes/blob/main/data/zip2fips.csv
+fixing data once imported into oracle
+update zip2fips set zipcode = '00' || zipcode where length(zipcode) = 3;
+update zip2fips set zipcode = '0' || zipcode where length(zipcode) = 4;
+ALTER TABLE ZIP2FIPS ADD PRIMARY KEY (ZIPCODE);
+
+
+SELECT LPAD(ZIP2FIPS.FIPS,5,'0') FIPS, X.* FROM ZIP2FIPS, (
+SELECT SUBSTR(ARTCUST2.CUST_STORE_ZIP_CODE,1,5) CUST_STORE_ZIP_CODE
+, SUM (RSTRETL1.AMT_SOLD) AMT_SOLD
+FROM RSTRETL1, ARTCUST2
+WHERE ARTCUST2.CUST_CODE = RSTRETL1.CUST_CODE
+  AND ARTCUST2.CUST_STORE_NO = RSTRETL1.CUST_STORE_NO
+AND RSTRETL1.OPS_YYYYPP = '202406'
+GROUP BY SUBSTR(ARTCUST2.CUST_STORE_ZIP_CODE,1,5)
+) X WHERE ZIP2FIPS.ZIPCODE (+) = X.CUST_STORE_ZIP_CODE
+
+https://stackoverflow.com/questions/23560996/return-results-of-a-sql-query-as-json-in-oracle-12c
+
+select JSON_OBJECT(FIPS, AMT_SOLD) from (
+SELECT LPAD(ZIP2FIPS.FIPS,5,'0') FIPS, SUM (X.AMT_SOLD) AMT_SOLD FROM ZIP2FIPS, (
+SELECT SUBSTR(ARTCUST2.CUST_STORE_ZIP_CODE,1,5) CUST_STORE_ZIP_CODE
+, SUM (RSTRETL1.AMT_SOLD) AMT_SOLD
+FROM RSTRETL1, ARTCUST2
+WHERE ARTCUST2.CUST_CODE = RSTRETL1.CUST_CODE
+  AND ARTCUST2.CUST_STORE_NO = RSTRETL1.CUST_STORE_NO
+AND RSTRETL1.OPS_YYYYPP = '202406'
+GROUP BY SUBSTR(ARTCUST2.CUST_STORE_ZIP_CODE,1,5)
+) X WHERE ZIP2FIPS.ZIPCODE (+) = X.CUST_STORE_ZIP_CODE
+GROUP BY LPAD(ZIP2FIPS.FIPS,5,'0')
+)
+
+
+
+
+
+
+We may have to subscribe to a database
+https://www.zip-codes.com/zip-code-database.asp
+
+
+from here I downloaded 12 files saved in the assets\hud folder
+this one might be a better source to use for zip to fips
+but it has an issue where the same zip code maps to multiple FIPS
+https://www.huduser.gov/apps/public/uspscrosswalk/home
--- src/app/choropleth/choropleth.component.ts
+++ src/app/choropleth/choropleth.component.ts
@@ -23,7 +23,7 @@ export class ChoroplethComponent implements OnInit {
     this.tooltip = d3.select('#tooltip');
   }
 
-  private drawMap(): void {
+  async drawMap() {
     const width = 960;
     const height = 600;
 
@@ -36,26 +36,103 @@ export class ChoroplethComponent implements OnInit {
     const path = d3.geoPath().projection(projection);
     
 
+    // this site gave us
+    // https://github.com/plotly/datasets/blob/95672208c26b44a6e32363b17a35b8caa1b5d2ef/geojson-counties-fips.json
+
+    // counties.json did NOT come from this resource - but this source looks like it might have worked
+    // the file came down with the following name: gz_2010_us_050_00_500k.json
+    // https://eric.clst.org/tech/usgeojson/  gold page
+
+    // counties.json came from this resource
+    // https://gist.githubusercontent.com/sdwfrost/d1c73f91dd9d175998ed166eb216994a/raw/e89c35f308cee7e2e5a784e1d3afc5d449e9e4bb/counties.geojson
+    // filename cb_2017_us_county_20m
+    // https://github.com/sdwfrost/us-maps
+    // https://www.census.gov/geographies/mapping-files/2017/geo/carto-boundary-file.html
+
     // { "type": "Feature", "properties": { "STATEFP": "36", "COUNTYFP": "103", "COUNTYNS": "00974149", "AFFGEOID": "0500000US36103", "GEOID": "36103", "NAME": "Suffolk", "LSAD": "06", "ALAND": 2360846288, "AWATER": 3785546967 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -72.018926, 41.274113999064525 ], [ -71.926802, 41.290121999064453 ], [ -71.917281, 41.251332999064616 ], [ -72.034754, 41.234817999064688 ], [ -72.018926, 41.274113999064525 ] ] ], [ [ [ -73.485365, 40.946396999065968 ], [ -73.436664, 40.934896999066027 ], [ -73.392862, 40.955296999065929 ], [ -73.33136, 40.92959699906605 ], [ -73.235827406678496, 40.906689766598454 ], [ -73.229285, 40.905120999066156 ], [ -73.148994, 40.928897999066052 ], [ -73.144673, 40.955841999065925 ], [ -73.110368, 40.971937999065851 ], [ -73.040445, 40.964497999065891 ], [ -72.859831, 40.966087999065884 ], [ -72.708069, 40.977850999065836 ], [ -72.585327, 40.997586999065732 ], [ -72.504305, 41.04332899906553 ], [ -72.445242, 41.086115999065335 ], [ -72.389809, 41.108303999065235 ], [ -72.354123, 41.139951999065097 ], [ -72.291109, 41.15587399906503 ], [ -72.189163, 41.193548999064873 ], [ -72.182033, 41.178344999064933 ], [ -72.254704, 41.110851999065233 ], [ -72.283093, 41.067873999065426 ], [ -72.217476, 41.040610999065542 ], [ -72.162898, 41.053186999065488 ], [ -72.126704, 41.115138999065216 ], [ -72.084207, 41.101523999065272 ], [ -72.095711, 41.054019999065481 ], [ -72.051928, 41.020505999065627 ], [ -71.959595, 41.071236999065412 ], [ -71.919385, 41.080516999065367 ], [ -71.856214, 41.070597999065406 ], [ -71.936977, 41.006136999065696 ], [ -72.097369, 40.958879999065907 ], [ -72.298727, 40.90315099906617 ], [ -72.39585, 40.866659999066343 ], [ -72.757176, 40.764370999066827 ], [ -72.923214, 40.713281999067071 ], [ -73.012545, 40.679650999067221 ], [ -73.146080869210792, 40.646407967168692 ], [ -73.20844, 40.630883999067464 ], [ -73.306396, 40.620755999067526 ], [ -73.351465, 40.630499999067474 ], [ -73.423976617557202, 40.613244876627064 ], [ -73.423253, 40.670919999067273 ], [ -73.436006, 40.73945799906695 ], [ -73.4385, 40.751310999066881 ], [ -73.462259, 40.86670999906633 ], [ -73.497351038626277, 40.923182272360464 ], [ -73.485365, 40.946396999065968 ] ] ] ] } },
     // d3.json('assets/counties-albers-10m').then((data: any) => {
-    d3.json('assets/counties.json').then((data: any) => {
+      
+    let features: any[] = [];
+    let FIPS: any = {};
+    let zip2FIPS: any[] = [];
+    let ZIPs: any = {}
+    await d3.json('assets/cb_2017_us_county_20m.json').then((data: any) => {
+      features = data.features;
+    })
+    await d3.json('assets/state-fips-xref.json').then((data: any) => {
+      FIPS = data;
+    })
+    await d3.json('assets/data.json').then((data: any) => {
+      zip2FIPS = data;
+    })
+    // zip2fFIPS.map((ZIP: any) => {
+    //   ZIPs = {...ZIPs, ZIP.FIPS: {"AMT_SOLD": ZIP.AMT_SOLD }}
+    // })
+
+    features = features.map((feature: any) => {
+      let STATEFP = feature.properties.STATEFP;
+      let state = FIPS[STATEFP] || {abbreviation: '??', name: '?'}
+      let STATEFP_code: string = feature.properties.STATEFP;
+      if (STATEFP_code.length === 4) {STATEFP_code = "0" + STATEFP_code}
+      let FIPS_code = feature.properties.STATEFP + feature.properties.COUNTYFP;
+      let zip2FIPS_record = zip2FIPS.find (x => { return x.FIPS === FIPS_code})
+      
+      let AMT_SOLD = 0;
+      if (zip2FIPS_record) {
+        AMT_SOLD = zip2FIPS_record.AMT_SOLD
+      }
+      return {...feature, state, data: AMT_SOLD}
+    })
+
+    console.log({features});
+    console.log({FIPS});
+    console.log({zip2FIPS});
+
+    // https://stackoverflow.com/questions/41848677/how-to-make-a-color-scale-in-d3-js-to-use-in-fill-attribute
+
+    let maxData = d3.max(features, function(d) {return d.data;});
+    let minData = d3.min(features, function(d) {return d.data;});
+    // if (minData < 0) {minData = 0}
+
+    console.log({minData, maxData})
+
+    let colorCodes = ["#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598", "#FFFFBF", "#FEE08B", "#FDAE61", "#F46D43", "#D53E4F", "#9E0142"];
+
+    // let colors = d3.scaleQuantile()
+    // .domain([minData,maxData])
+    // .range(colorCodes);
+    
+    let color = d3.scaleLinear<string, number>()
+      .domain([minData, maxData])
+      .range(["red", "blue"]);
+
+    // color: {scheme: "Blues", type: "quantize", n: 9, domain: [1, 10], label: "Unemployment rate (%)", legend: true},
+
+    // .attr('fill', '#abc')
+
+    // d3.json('assets/counties.json').then((data: any) => {
+    // d3.json('assets/cb_2017_us_county_20m.json').then((data: any) => {
       this.svg.selectAll('path')
-        .data(data.features)
+        // .data(data.features)
+        .data(features)
         .enter().append('path')
         .attr('d', path)
-        .attr('fill', '#ccc')
+        .attr('fill', function (d:any, i:number) {
+          // console.log({event, d})
+          return color(d.data)})
         .attr('stroke', '#fff')
         .on('mouseover', (event: any, d: any) => {
           this.tooltip.transition().duration(200).style('opacity', 0.9);
           // this.tooltip.html(`County: ${d.properties.NAME}<br>Value: ${d.properties.density}`)
-          this.tooltip.html(`County: ${d.properties.NAME}<br>STATEFP: ${d.properties.STATEFP}`)
+          // this.tooltip.html(`County: ${d.properties.NAME}<br>STATEFP: ${d.properties.STATEFP}`)
+          this.tooltip.html(`County: ${d.properties.NAME}<br>State: ${d.state.name}<br>Sales: ${d.data}`)
             .style('left', (event.pageX + 5) + 'px')
             .style('top', (event.pageY - 28) + 'px');
         })
         .on('mouseout', () => {
           this.tooltip.transition().duration(500).style('opacity', 0);
         });
-    });
+    // });
 
     console.log(this.svg)
   }
