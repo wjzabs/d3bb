623d72e31ff76ad47b60fd56754e749ddfd4e808
Walter J. Zielenski <wjz@absolution.com>
2024-11-19 13:58:10 -0500

    change to absolution col names

--- src/app/stacked/stacked.component.ts
+++ src/app/stacked/stacked.component.ts
@@ -12,11 +12,12 @@ import {
 
   
 export interface StackedDataItem {
-    position: number,
-    name: string,
-    category: string,
-    bar: string,
-    value: number,
+    // position: number,
+    CATEGORY_DESC: string,
+    CATEGORY_CODE: string,
+    BAR_CODE: string,
+    BAR_DESC: string,
+    VALUE: number,
     color?: string
 }
 
@@ -36,39 +37,14 @@ export interface StackedDataItem {
     @Input("legendSubtitle") legendSubtitle: string = 'legendSubtitle';
     public chartId: string;
     constructor(private d3: D3Service) {
-      this.chartId = this.d3.generateId(5);
-
-    // {name: 'Shamir', category: 'SHAMIR', value: 0.11728827117288271}
-
-    //   this.data = [
-    //     { position: 0, name: "Pie 1", value: 10 },
-    //     { position: 1, name: "Pie 2", value: 20 },
-    //     { position: 2, name: "Pie 3", value: 30 },
-    //     { position: 3, name: "Pie 4", value: 40 },
-    //     { position: 4, name: "Pie 5", value: -50 },
-    //     { position: 5, name: "Pie 6", value: 60 },
-    //     { position: 6, name: "Pie 7", value: 70 },
-    //     { position: 7, name: "Pie 8", value: 80 },
-    //     { position: 8, name: "Pie 9", value: 90 },
-    //     { position: 9, name: "Pie 10", value: -100 },
-    //     { position: 10, name: "Pie 11", value: 110 },
-    //     { position: 11, name: "Pie 12", value: -120 },
-    //     { position: 12, name: "Pie 13", value: 130 },
-    //     { position: 13, name: "Pie 14", value: 140 },
-    //     { position: 14, name: "Pie 15", value: 150 },
-    //     { position: 15, name: "Pie 16", value: 100 },
-    //     { position: 16, name: "Pie 16", value: 60 },
-    //     { position: 17, name: "Pie 16", value: -60 }
-    //   ];
-     
+      this.chartId = this.d3.generateId(5);     
     }
   
     async ngAfterViewInit() {
-      await this.createDiverginChart();
+      await this.createDivergingChart();
     }
   
-    async createDiverginChart() {
-
+    async createDivergingChart() {
 
   const categories = await <Promise<any[]>>this.d3.d3.json("assets/stacks-categories.json")
   console.table(categories)
@@ -89,13 +65,12 @@ export interface StackedDataItem {
   console.log(signs)
 
 //   const bias = this.d3.d3.sort(
-//     this.d3.d3.rollup(this.data, v => this.d3.d3.sum(v, (d:any) => {return d.value * Math.min(0, (signs.get(d.category) as number)) }), (d:any) => d.name),
+//     this.d3.d3.rollup(this.data, v => this.d3.d3.sum(v, (d:any) => {return d.VALUE * Math.min(0, (signs.get(d.category) as number)) }), (d:any) => d.name),
 //     ([, a]) => a
 //   );
 
 this.data.forEach((d:StackedDataItem) => {
-    // console.log({d, sign: signs.get(d.category), value: d.value, calc: Math.abs(d.value) * (signs.get(d.category) as number)})
-    d.value = Math.abs(d.value) * (signs.get(d.category) as number)
+    d.VALUE = Math.abs(d.VALUE) * (signs.get(d.CATEGORY_CODE) as number)
 })
 
       var margin = { top: 40, right: 50, bottom: 60, left: 50 };
@@ -167,7 +142,7 @@ this.data.forEach((d:StackedDataItem) => {
   
       y.domain(
         this.data.map(function(d) {
-          return d.name;
+          return d.CATEGORY_DESC;
         })
       );
 
@@ -175,15 +150,15 @@ this.data.forEach((d:StackedDataItem) => {
 
       // https://stackoverflow.com/questions/29493843/finding-d3-extent-of-multiple-values-in-object-array
       x.domain(
-        this.d3.d3.extent(this.data.map((x:any) => {return x.value})) as [number,number]
+        this.d3.d3.extent(this.data.map((x:StackedDataItem) => {return x.VALUE})) as [number,number]
         // extent
         // this.d3.d3.extent(this.data, function(d) {
         // return d.value;
         // })          
       );
   
-      var max = this.d3.d3.max(this.data, function(d) {
-        return d.value;
+      var max = this.d3.d3.max(this.data, (d:StackedDataItem) => {
+        return d.VALUE;
       });
       colour.domain([-(max || 0), max || 0]);
     //   colour.domain([-max, max]);
@@ -213,24 +188,24 @@ this.data.forEach((d:StackedDataItem) => {
         .append("rect")
         .attr("class", "annual-growth")
         .attr("x", function(d) {
-          return x(Math.min(0, d.value));
+          return x(Math.min(0, d.VALUE));
         })
-        .attr("y", (d:any) => { return y(d.name) as number })
+        .attr("y", (d:StackedDataItem) => { return y(d.CATEGORY_DESC) as number })
         // .attr("y", function(d) {
         //   return y(d.name);
         // })
         .attr("height", y.bandwidth())
-        .attr("width", function(d) {
-          return Math.abs(x(d.value) - x(0));
+        .attr("width", (d:StackedDataItem) => {
+          return Math.abs(x(d.VALUE) - x(0));
         })
-        .style("fill", function(d) {
-          return d.color ? d.color : colour(d.value);
+        .style("fill", (d:StackedDataItem) => {
+          return d.color ? d.color : colour(d.VALUE);
         })
         .append("title")
         // .text(({category, data: [name, value]}) => `${name}
         // ${formatValue(value.get(category))} ${category}`);
-        .text(({category, name, bar, value}) => `${name}
-        ${formatValue(value)} ${bar}`);
+        .text(({CATEGORY_CODE, CATEGORY_DESC, BAR_CODE, VALUE}) => `${CATEGORY_DESC}
+        ${formatValue(VALUE)} ${BAR_CODE}`);
   
       var labels = svg.append("g").attr("class", "labels");
   
@@ -241,16 +216,16 @@ this.data.forEach((d:StackedDataItem) => {
         .append("text")
         .attr("class", "bar-label")
         .attr("x", x(0))
-        .attr("y", (d:any) => { return y(d.name) as number })
-        .attr("dx", function(d) {
-          return d.value < 0 ? cfg.labelMargin : -cfg.labelMargin;
+        .attr("y", (d:StackedDataItem) => { return y(d.CATEGORY_DESC) as number })
+        .attr("dx", (d:StackedDataItem) => {
+          return d.VALUE < 0 ? cfg.labelMargin : -cfg.labelMargin;
         })
         .attr("dy", y.bandwidth())
-        .attr("text-anchor", function(d) {
-          return d.value < 0 ? "start" : "end";
+        .attr("text-anchor", (d:StackedDataItem) => {
+          return d.VALUE < 0 ? "start" : "end";
         })
-        .text(function(d) {
-          return d.name;
+        .text((d:StackedDataItem) => {
+          return d.CATEGORY_DESC;
         });
       /* example of selective fill
         .style("fill", function (d) {
@@ -314,7 +289,7 @@ stacks_get_data(selectedCATEGORY_CODE: string, categories: any[], dataset: any[]
     let data: any[] = []
     dataset.forEach((d:any) => {
       let c = categories.find((x:any) => {return x.CATEGORY_CODE === d.CATEGORY_CODE})
-      let o = {name: c.CATEGORY_DESC, category: d.CATEGORY_CODE, bar: d.BAR_CODE, value: +d.PCT}
+      let o = {CATEGORY_DESC: c.CATEGORY_DESC, CATEGORY_CODE: d.CATEGORY_CODE, BAR_CODE: d.BAR_CODE,  BAR_DESC: d.BAR_DESC, VALUE: +d.PCT}
       data.push(o)
     })
   
@@ -322,9 +297,9 @@ stacks_get_data(selectedCATEGORY_CODE: string, categories: any[], dataset: any[]
   
     // Normalize absolute values to percentage.
     this.d3.d3.rollup(data, group => {
-      const sum = this.d3.d3.sum(group, d => d.value);
-      for (const d of group) d.value /= sum;
-    }, d => d.name);
+      const sum = this.d3.d3.sum(group, (d:StackedDataItem) => d.VALUE);
+      for (const d of group) d.VALUE /= sum;
+    }, (d:StackedDataItem) => d.CATEGORY_DESC);
   
     // let result = Object.assign(data, {
     //   columns: ["speaker", "ruling", "count"],
@@ -339,7 +314,7 @@ stacks_get_data(selectedCATEGORY_CODE: string, categories: any[], dataset: any[]
     // });
   
     let result = {data: Object.assign(data), options: {
-      columns: ["name", "category", "value"],
+      columns: ["CATEGORY_DESC", "CATEGORY_CODE", "VALUE"], // WHAT IS THIS FOR?
       negative: "← Others",
       positive: selectedCATEGORY_CODE + " →",
       negatives: [...categories
